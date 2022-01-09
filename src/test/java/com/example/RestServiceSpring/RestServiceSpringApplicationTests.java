038b82349638a372538634692e692da494d69982
felya_97@mail.ru <felya_97@mail.ru>
2022-01-09 10:57:37 +0600

    First commit

---
+++ src/test/java/com/example/RestServiceSpring/RestServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestServiceSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
