15d69093312acdf093bdf96d08a5be39c45c847c
felya_97@mail.ru <felya_97@mail.ru>
2022-01-09 11:18:39 +0600

    Worked RestFull Service

--- src/main/java/com/example/RestServiceSpring/RestServiceSpringApplication.java
+++ src/main/java/com/example/RestServiceSpring/RestServiceSpringApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestServiceSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestServiceSpringApplication.class, args);
+
+
 	}
 
 }
